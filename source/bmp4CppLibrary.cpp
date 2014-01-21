621743e0552b56e1504f3c09bc995380394b2995
Vincent Berthiaume <vberthiaume@gmail.com>
2014-01-21 12:42:19 -0500

    initial xcode commit

--- source/bmp4CppLibrary.cpp
+++ source/bmp4CppLibrary.cpp
@@ -3,7 +3,7 @@
 // Created by   : BMP4 (Vincent Berthiaume)
 // Description  : General c++ library
 //
-// © 2014, Vincent Berthiaume, All Rights Reserved
+// ï¿½ 2014, Vincent Berthiaume, All Rights Reserved
 //-------------------------------------------------------------------------------------------------------	
 
 #include "bmp4CppLibrary.h"
@@ -16,6 +16,5 @@ using namespace std;
 void checkLibraryLoaded()
 {
 	cout << "BMP4 Cpp Library loaded correctly!\n\n\n";
-	
 }
 
---
+++ source/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  bmp4CppLibrary
+//
+//  Created by Nicolai on 1/21/2014.
+//  Copyright (c) 2014 BMP4. All rights reserved.
+//
+
+#include <iostream>
+#include "bmp4CppLibrary.h"
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    checkLibraryLoaded();
+    return 0;
+}
+
